2cd7b7baf7236fc2234105f1578573a853895fd7
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2016-10-17 16:11:21 +0300

    test rename

--- test/string-calculalator.test.js
+++ test/string-calculalator.test.js
@@ -1,8 +1,8 @@
 var assert = require('chai').assert;
 var Calculator = require('../src/string-calculator');
 
-describe('string calculator', function() {
-    it('should return default value if input is empty', function() {
+describe('string calculator should', function() {
+    it('return default value if input is empty', function() {
         var calculator = createCalculator();
 
         var result = calculator.add("");
@@ -10,7 +10,7 @@ describe('string calculator', function() {
         assert.equal(result, 0);
     });
 
-    it('should return single number if input is that single number', function() {
+    it('return single number if input is that single number', function() {
         var calculator = createCalculator();
 
         var sum = calculator.add('1');
@@ -18,7 +18,7 @@ describe('string calculator', function() {
         assert.equal(sum, 1);
     });
 
-    it('should return any single numbers if input is that single number', function() {
+    it('return any single numbers if input is that single number', function() {
         var calculator = createCalculator();
 
         var sum = calculator.add('2')
